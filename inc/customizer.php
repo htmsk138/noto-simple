d1643628e0c259f6511e72dfb89657423b68a3ea
Hitomi <hitomi0528@gmail.com>
2017-08-30 16:20:40 +0900

    Underscore S

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Japanese Simple Theme Customizer
+ *
+ * @package Japanese_Simple
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function japanese_simple_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'japanese_simple_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'japanese_simple_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'japanese_simple_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function japanese_simple_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function japanese_simple_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function japanese_simple_customize_preview_js() {
+	wp_enqueue_script( 'japanese-simple-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'japanese_simple_customize_preview_js' );
