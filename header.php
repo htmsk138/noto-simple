e0091dbe1f56ed91018a7d7c3e30601c6b2ba6c8
Hitomi <hitomi0528@gmail.com>
2019-02-20 19:18:10 +0900

    Ver. 1.1.1

--- header.php
+++ header.php
@@ -49,8 +49,8 @@
 
             <nav id="site-navigation" class="main-navigation">
                 <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                <i class="material-icons open"><?php esc_html_e('Menu', 'noto-simple'); ?></i>
-					<i class="material-icons close"><?php esc_html_e('Close', 'noto-simple'); ?></i>
+                <i class="material-icons open"><?php esc_html_e('menu', 'noto-simple'); ?></i>
+					<i class="material-icons close"><?php esc_html_e('close', 'noto-simple'); ?></i>
 				</button>
                 <?php
                     wp_nav_menu( array(
