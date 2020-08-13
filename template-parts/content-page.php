3ad2c8b988a089469b19164c83cdd3a93326054b
Hitomi Seki <>
2020-08-13 10:51:09 +0900

    Ver. 1.2.1 (fix #1)

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,6 +14,12 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="featured-image">
+	<?php the_post_thumbnail(); ?>
+	</div><!-- .featured-image -->
+	<?php endif; ?>
+
 	<div class="entry-content">
 		<?php
 			the_content();
